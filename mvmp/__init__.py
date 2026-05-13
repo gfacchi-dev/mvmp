64cdaef0c6a4a19db192a1d27c18e5007ac15c85
GitHub Action <action@github.com>
2026-05-13 16:12:45 +0000

    Bump version to v1.0.0

--- mvmp/__init__.py
+++ mvmp/__init__.py
@@ -5,5 +5,5 @@ Facial landmark detection for 3D meshes
 
 from .core.facemarker import Facemarker, FacemarkerResult
 
-__version__ = "0.2.13"
+__version__ = "1.0.0"
 __all__ = ["Facemarker", "FacemarkerResult"]
